c2d77ff5430f0f7de559c88af421360f2979ae55
unknown <joel.epstein2@gmail.com>
2017-11-09 20:17:20 -0800

    finished

---
+++ index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var server = require("./server");
+var router = require("./router");
+var requestHandlers = require("./module/requestHandlers");
+
+var handle = {};
+handle["/"] = requestHandlers.start;
+handle["/start"] = requestHandlers.start;
+handle["/upload"] = requestHandlers.upload;
+
+server.start(router.route);
